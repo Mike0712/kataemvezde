bb1eb722d07b19f0078b2257cd9c6c8305e1192a
Mikhaul Churikov <barrister2003@gmail.com>
2016-10-28 00:01:04 +0300

    Миграции модели начало

---
+++ database/migrations/2016_10_27_200102_create_persons_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('sex', ['male', 'female']);
+            $table->timestamp('year_birth');
+            $table->integer('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                    ->references('id')->on('users')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persons');
+    }
+}
