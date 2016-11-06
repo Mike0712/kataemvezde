67e4ff2ee626aec2ce3295827dc13336285ed621
Михаил Чуриков <barrister2003@gmail.com>
2016-11-06 20:41:24 +0300

    Добавил первую вьюху, без рендеринга каких-либо данных

---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.main')
+
+@section('content')
+    <!-- Content -->
+    <div class="content"><div class="ic"></div>
+        <div class="container">
+            <div class="col-lg-12">
+                <h2 class="center">Ближайшее мероприятие</h2>
+                <div class="count_wrap">
+                    <div id="counter"></div>
+                    <div class="clear"></div>
+                    <a href="#">Записаться</a>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="col-lg-4">
+                <div class="box">
+                    <div class="box_title">Календарь</div>
+                    <div class="box_bot">
+                        <div class="maxheight">
+                            <img src="/images/page1_img1.jpg" alt=""><a href="#">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="box">
+                    <div class="box_title">Участники</div>
+                    <div class="box_bot">
+                        <div class="maxheight">
+                            <img src="/images/page1_img2.jpg" alt=""><a href="#">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="box">
+                    <div class="box_title">Результаты</div>
+                    <div class="box_bot">
+                        <div class="maxheight">
+                            <img src="/images/page1_img3.jpg" alt=""><a href="#">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="col-lg-7">
+                <h3>Welcome to Our Club</h3>
+                <img src="/images/page1_img4.jpg" alt="" class="img_inner fleft">
+                <div class="extra_wrapper">
+                    <p>Follow the link to learn more about this <span class="color1"><a href="http://blog.templatemonster.com/free-website-templates/" rel="nofollow">free theme</a></span> produced by TemplateMonster. </p>
+                    <p>More <span class="color1"><a href="http://www.templatemonster.com/properties/topic/society-people/" rel="nofollow">themes of this kind</a></span> are to be found at TemplateMonster’s website</p>
+                    Vivamus at magna non nunc tristique rhoncuseri tym.
+                </div>
+                <div class="clear sep__1"></div>
+                Gamus at magna non nunc tristique rhoncuseri tym. Aliquam nibh ante, egestas id dictum aterert commodo re luctus libero. Praesent faucibus malesuada cibuste. Donec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur orci sed Curabitur vel lorem sit amet nulla ullamcorper fermentum. In vitae varius augue, eu consectetur ligula. Etiam dui eros, laoreet sit amet est vel, commodo venenatis eros.
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-4">
+                <h3>Latest News</h3>
+                <div class="news">
+                    <time datetime="2014-01-01">14<span>MAR</span></time>
+                    <div class="extra_wrapper">
+                        <p class="color1"><a href="#">Magna non nunc tristique rhoncuseri </a></p>
+                        Aliquam nibh ante, egestas id dictum a, commodo re luctus liberaesenucibus malesuada cibuste.
+                    </div>
+                </div>
+                <div class="news">
+                    <time datetime="2014-01-01">28<span>MAR</span></time>
+                    <div class="extra_wrapper">
+                        <p class="color1"><a href="#">Tagna non nunc tristique rhoncuserr </a></p>
+                        Boliquam nibh ante, egestas id dictum a, commodo re luctus liberaesenucibus malesuada cibustew.
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="container">
+                <div class="hor_separator"></div>
+                <h3 class="head1 center">We Will Never Stop</h3>
+            </div>
+            <div class="clear"></div>
+            <div class="container">
+                <div id="grid" class="main">
+                    <div class="view">
+                        <div class="view-back">
+                            <span data-icon="" class="fa fa-eye"><span>110</span></span>
+                            <span data-icon="" class="fa fa-heart-o"><span>60</span></span>
+                            <a href="#">&rarr;</a>
+                        </div>
+                        <img src="/images/page1_img5.jpg" />
+                    </div>
+                    <div class="view">
+                        <div class="view-back">
+                            <span data-icon="" class="fa fa-eye"><span>208</span></span>
+                            <span data-icon="" class="fa fa-heart-o"><span>813</span></span>
+                            <a href="#">&rarr;</a>
+                        </div>
+                        <img src="/images/page1_img6.jpg" />
+                    </div>
+                    <div class="view">
+                        <div class="view-back">
+                            <span data-icon="" class="fa fa-eye"><span>51</span></span>
+                            <span data-icon="" class="fa fa-heart-o"><span>88</span></span>
+                            <a href="#">&rarr;</a>
+                        </div>
+                        <img src="/images/page1_img7.jpg" />
+                    </div>
+                    <div class="clear"></div>
+                    <div class="view">
+                        <div class="view-back">
+                            <span data-icon="" class="fa fa-eye"><span>228</span></span>
+                            <span data-icon="" class="fa fa-heart-o"><span>62</span></span>
+                            <a href="#">&rarr;</a>
+                        </div>
+                        <img src="/images/page1_img8.jpg" />
+                    </div>
+                    <div class="view">
+                        <div class="view-back">
+                            <span data-icon="" class="fa fa-eye"><span>117</span></span>
+                            <span data-icon="" class="fa fa-heart-o"><span>368</span></span>
+                            <a href="#">&rarr;</a>
+                        </div>
+                        <img src="/images/page1_img9.jpg" />
+                    </div>
+                    <div class="view">
+                        <div class="view-back">
+                            <span data-icon="" class="fa fa-eye"><span>22</span></span>
+                            <span data-icon="" class="fa fa-heart-o"><span>38</span></span>
+                            <a href="#">&rarr;</a>
+                        </div>
+                        <img src="/images/page1_img10.jpg" />
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+@endsection
