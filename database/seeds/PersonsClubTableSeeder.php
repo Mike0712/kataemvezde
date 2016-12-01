c5db0dd96ea7a2adec62e2dfb45b9eff1a86fa1e
Mike <m.churikov@it-struktura.ru>
2016-12-01 12:48:58 +0300

    pivot таблица person_club и сид

---
+++ database/seeds/PersonsClubTableSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PersonsClubTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('person_club')->insert([
+            'person_id' => 1,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 2,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 3,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 3,
+            'club_id' => 2,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 4,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 5,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 6,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 7,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 8,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 9,
+            'club_id' => 1,
+        ]);
+    
+        DB::table('person_club')->insert([
+            'person_id' => 10,
+            'club_id' => 1,
+        ]);
+    }
+}
